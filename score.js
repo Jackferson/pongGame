94b2d90c8d8ff3be1ced70707f98ad192d469d40
Jack <Jackferson15@gmail.com>
2023-01-09 12:35:35 -0300

    add: score value

--- score.js
+++ score.js
@@ -19,21 +19,36 @@ class Score {
     ball.y = ball.restartValues.y;
     ball.angle = ball.restartValues.angle;
     ball.speed = ball.restartValues.speed;
-    gameControls.pauseGame()
+    gameControls.pauseGame();
+    this.printPoints();
   }
 
+  printPoints() {
+    const left = document.getElementById("leftside");
+    const right = document.getElementById("rightside");
+    const leftSpan = document.getElementById("leftValue");
+    const rightSpan = document.getElementById("rightValue");
+
+    right.innerHTML = "";
+    left.innerHTML = "";
+    leftSpan.textContent = this.player1Points;
+    rightSpan.textContent = this.player2Points;
+
+    for (let i = 0; i < this.player2Points; i++) {
+      right.innerHTML += "&#x1F94E;";
+    }
+    for (let i = 0; i < this.player1Points; i++) {
+      left.innerHTML += "&#x1F94E;";
+    }
+  }
   #pointDetector() {
     if (ball.x >= table.limits[1][0].x) {
       this.player1Points++;
-      console.log("player1 points: " + this.player1Points);
       this.#newPoint();
     }
     if (ball.x <= table.limits[0][0].x) {
       this.player2Points++;
-      console.log("player2 points: " + this.player2Points);
       this.#newPoint();
     }
   }
 }
-
-//  
